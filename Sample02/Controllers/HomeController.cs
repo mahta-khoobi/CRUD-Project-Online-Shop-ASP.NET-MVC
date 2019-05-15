d9de799529642b4e39a3d6ee87860a306ae9a541
Mahta.khoobi <Mahta.khoobi@yahoo.com>
2019-05-15 22:00:45 +0430

    Home controller added

---
+++ Sample02/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sample02.Controllers
+{
+    public class HomeController : Controller
+    {
+        #region [-ctor-]
+        public HomeController()
+        {
+
+        } 
+        #endregion
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
