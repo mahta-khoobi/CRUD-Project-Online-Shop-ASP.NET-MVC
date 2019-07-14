8813153c8fbd8a8afcb74f6ee3106748c9c00e22
Mahta.khoobi <Mahta.khoobi@yahoo.com>
2019-07-14 23:41:35 +0430

    ProductCategoryRepository commited and a helper class named CategorySelectHelper added

--- Sample02/Models/DomainModels/POCO/ProductCategoryRepository.cs
+++ Sample02/Models/DomainModels/POCO/ProductCategoryRepository.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Sample02.Models.Helper.SPHelper.ProductCategory;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace Sample02.Models.DomainModels.POCO
 {
-    public class ProductCategoryRepository : Models.Framework.Base.BaseRepository<Models.DomainModels.DTO.EF.OnlineShopEntities, Models.DomainModels.DTO.EF.ProductCategory, int>
+    public class ProductCategoryRepository : Models.Framework.Base.BaseRepository<Models.DomainModels.DTO.EF.OnlineShopEntities, Models.DomainModels.DTO.EF.ProductCategory, int,CategorySelectHelper>
     {
         #region [- ctor -]
         public ProductCategoryRepository(Models.DomainModels.DTO.EF.OnlineShopEntities context) : base(context)
---
+++ Sample02/Models/Helper/SPHelper/ProductCategory/CategorySelectHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sample02.Models.Helper.SPHelper.ProductCategory
+{
+    public class CategorySelectHelper
+    {
+        public int Id { get; set; }
+        public int CategoryCode { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
