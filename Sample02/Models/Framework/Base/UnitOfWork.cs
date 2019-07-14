87eba958466f86b6e0607af029b8a9846d6f923b
Mahta.khoobi <Mahta.khoobi@yahoo.com>
2019-07-14 23:27:29 +0430

    Unit of work commited

--- Sample02/Models/Framework/Base/UnitOfWork.cs
+++ Sample02/Models/Framework/Base/UnitOfWork.cs
@@ -6,18 +6,19 @@ using System.Web;
 
 namespace Sample02.Models.Framework.Base
 {
-    public class UnitOfWork<K_DbContext, T_Entity, U_PrimaryKey>
+    public class UnitOfWork<K_DbContext, T_Entity, U_PrimaryKey, T_ViewHelper>
                                                                  where K_DbContext : DbContext
-                                                                where T_Entity : class
+                                                                 where T_Entity : class
+                                                                 where T_ViewHelper : class
 
     {
         //IUnitOfWork aggregated
         #region [- ctor -] 
-        public UnitOfWork(Abstract.IUnitOfWork<K_DbContext, T_Entity, U_PrimaryKey> ref_IUnitOfWork)
+        public UnitOfWork(Abstract.IUnitOfWork<K_DbContext, T_Entity, U_PrimaryKey,T_ViewHelper> ref_IUnitOfWork)
         {
             Ref_IUnitOfWork = ref_IUnitOfWork;
         }
         #endregion
-        public Models.Framework.Abstract.IUnitOfWork<K_DbContext, T_Entity, U_PrimaryKey> Ref_IUnitOfWork { get; set; }
+        public Models.Framework.Abstract.IUnitOfWork<K_DbContext, T_Entity, U_PrimaryKey,T_ViewHelper> Ref_IUnitOfWork { get; set; }
     }
 }
