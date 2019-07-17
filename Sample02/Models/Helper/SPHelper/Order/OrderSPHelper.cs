3703d948ffb532f0d5fa217db6dfe846c8e58d01
Mahta.khoobi <Mahta.khoobi@yahoo.com>
2019-07-17 12:59:52 +0430

    OrderSPHelper added to helper folder

---
+++ Sample02/Models/Helper/SPHelper/Order/OrderSPHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sample02.Models.Helper.SPHelper.Order
+{
+    public class OrderSPHelper
+    {
+       
+        public const string usp_Order_Select = "[dbo].[usp_Order_Select]";
+        public const string usp_Order_Insert = "[dbo].[usp_Order_Insert] @orderMasterList , @orderDetailsList";
+
+
+    }
+}
