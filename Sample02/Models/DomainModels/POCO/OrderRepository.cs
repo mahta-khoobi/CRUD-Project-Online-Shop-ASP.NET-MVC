ff67a698d54428059183f475d6faf381c4bc1e29
Mahta.khoobi <Mahta.khoobi@yahoo.com>
2019-07-17 12:56:50 +0430

    OrderRepository class added to POCO

---
+++ Sample02/Models/DomainModels/POCO/OrderRepository.cs
@@ -0,0 +1,20 @@
+﻿using Sample02.Models.Helper.SPHelper.Order;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sample02.Models.DomainModels.POCO
+{
+    public class OrderRepository: Models.Framework.Base.BaseRepository<Models.DomainModels.DTO.EF.OnlineShopEntities, Models.DomainModels.DTO.EF.OrderMaster, System.Int32, OrderSelectHelper>
+    {
+
+
+        #region [-ctor-]
+        public OrderRepository(Models.DomainModels.DTO.EF.OnlineShopEntities context) :base(context)
+        {
+
+        } 
+        #endregion
+    }
+}
